c79efa2384a0918b6c44e43d5516b11500a2c2e6
Youssef Shabo <youssefshabo@gmail.com>
2023-09-29 11:28:04 -0500

    sept 28th

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const choiceList = ["Lapis", "Papyrus", "Scalpellus"];
 computerChoice = choiceList[Math.floor(Math.random()*3)];
 
 console.log("the computer chose " + computerChoice);
+
+let days = 30;
+
+while (days > 0) {
+    console.log(days + " days left!");
+    days = days -1;
+}
+console.log("it's finally here!");
+
